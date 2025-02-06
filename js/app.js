bd173641cb034a548b38430b99b1baf6bac32a87
Aline Soares <linesoares.s@hotmail.com>
2025-02-06 07:50:19 -0300

    SorteioAmigo

---
+++ js/app.js
@@ -0,0 +1,41 @@
+let participantes = [];
+let sorteados = [];
+let sorteio =[];
+let participantesHTML = document.getElementById('lista-amigos');
+
+
+function adicionar(){
+    let amigo = document.getElementById('nome-amigo');
+    participantes.push(amigo.value);
+    participantesHTML.innerHTML = participantes;
+    amigo.value = '';
+};
+
+function definirAmigoSecreto(lista){
+    let nomeAmigoSorteado = lista[parseInt(Math.random()*lista.length)];
+    return nomeAmigoSorteado;
+};
+
+
+function sortear(){
+    let resultadoSorteio = document.getElementById('lista-sorteio');
+    for(i=0; i < participantes.length; i++ ){
+    let amigoSecreto = definirAmigoSecreto(participantes);
+    while (amigoSecreto == participantes[i] || sorteados.includes(amigoSecreto) == true || amigoSecreto == null ){
+        amigoSecreto = definirAmigoSecreto(participantes);
+    }
+    sorteados.push(amigoSecreto);
+    sorteio.push([` 
+         ${participantes[i]} ---> ${amigoSecreto}`]); 
+    };
+    resultadoSorteio.innerHTML= sorteio;
+};
+
+
+function reiniciar(){
+    document.getElementById('lista-sorteio').innerHTML= '';
+    participantes = [];
+    sorteados = [];
+    sorteio =[];
+    participantesHTML.innerHTML = '';
+};
