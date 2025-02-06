cdac152b74a14336a5e1377d89900901b4175350
Aline Soares <linesoares.s@hotmail.com>
2025-02-06 08:33:13 -0300

    Adicionando validações

--- js/app.js
+++ js/app.js
@@ -6,16 +6,18 @@ let participantesHTML = document.getElementById('lista-amigos');
 
 function adicionar(){
     let amigo = document.getElementById('nome-amigo');
-    participantes.push(amigo.value);
-    participantesHTML.innerHTML = participantes;
-    amigo.value = '';
+    if (amigo.value == ''){
+        alert('Informação inválida!');
+    }else{
+        if(participantes.includes(amigo.value)){
+            alert('Este nome já está na lista!');            
+        }else{
+            participantes.push(amigo.value);
+            participantesHTML.innerHTML = participantes;
+            amigo.value = '';
+         };
+    };
 };
-
-/*function remover(){
-    let amigoRemovido = participantesHTML.click;
-    participantes = participantes.filter(amigoRemovido.value);
-    participantesHTML.innerHTML = participantes;
-};*/
 
 function definirAmigoSecreto(lista){
     let nomeAmigoSorteado = lista[parseInt(Math.random()*lista.length)];
@@ -25,15 +27,19 @@ function definirAmigoSecreto(lista){
 
 function sortear(){
     let resultadoSorteio = document.getElementById('lista-sorteio');
-    for(i=0; i < participantes.length; i++ ){
-    let amigoSecreto = definirAmigoSecreto(participantes);
-    while (amigoSecreto == participantes[i] || sorteados.includes(amigoSecreto) == true || amigoSecreto == null ){
-        amigoSecreto = definirAmigoSecreto(participantes);
-    }
-    sorteados.push(amigoSecreto);
-    sorteio.push([`${participantes[i]} ---> ${amigoSecreto}`]); 
+    if(participantes.length >= 4){
+        for(i=0; i < participantes.length; i++ ){
+            let amigoSecreto = definirAmigoSecreto(participantes);
+            while (amigoSecreto == participantes[i] || sorteados.includes(amigoSecreto) == true || amigoSecreto == null ){
+            amigoSecreto = definirAmigoSecreto(participantes);
+            }
+        sorteados.push(amigoSecreto);
+        sorteio.push([`${participantes[i]} ---> ${amigoSecreto}`]); 
+        };
+        resultadoSorteio.innerHTML= `${sorteio} <br>`;
+    }else{
+        alert('É necessário no mínimo 4 participantes para realizar o sorteio');
     };
-    resultadoSorteio.innerHTML= `${sorteio} <br>`;
 };
 
 
